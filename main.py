af5e22bfeaecb50366cd84b2fbdf6e8f3aa2cf89
naktheking <78290773+naktheking@users.noreply.github.com>
2024-10-22 17:27:50 -0500

    implemented API

--- main.py
+++ main.py
@@ -1,70 +1,8 @@
-
-# Inputs
-'''file_input = input("What weather file would you like to open?\n")
-
-# Data and File Init
-data = []
-
-
-# Parsing data to display it
-with open(file_input, 'r') as file:
-    for line in file:
-        line = line.strip()
-        data.append(line)
-
-
-for i in range(0, len(data)):
-    data[i] = data[i].split(" ")
-
-    data[i][0] = data[i][0].replace(":", "")
-
-# Data Type Input
-data_type = input("What data type would you like to get\n")
-data_index = -1
-
-# Find Data Type
-for i, row in enumerate(data):
-    if data_type in row:
-        data_index = i
-
-
-# Gets dates and their index
-first_date = input("From what date (inclusive)\n")
-end_date = input("To what date (inclusive)\n")
-
-first_date_index = data[0].index(first_date)
-end_date_index = data[0].index(end_date)
-
-display_type = input("What data would you like (Max)(Min)(All)")
-
-# Prints to the user data on given dates
-if display_type.lower() == "all":
-    for i in range(first_date_index, end_date_index + 1):
-        print(data[data_index][i])
-elif display_type.lower() == "min":
-    for i in range(1, len(data[data_index])):
-        minimum = data[data_index][1]
-        if float(data[data_index][i]) < float(minimum):
-            minimum = data[data_index][i]
-
-    print(minimum)
-elif display_type.lower() == "max":
-    for i in range(1, len(data[data_index])):
-        maximum = 0
-        if float(data[data_index][i]) > maximum:
-            maximum = data[data_index][i]
-
-    print(maximum)'''
-
-
-
-
-
-
 import requests
 import json
 import statistics
 import matplotlib.pyplot as plt
+import requests
 
 data = []
 
@@ -121,32 +59,52 @@ def get_data(first_date, end_date, display_type, data_type, data):
         plt.ylabel('Frequency')
         plt.show()
 
+def get_liveWeather_from_api(api_key, lat, lon, time):
+    base_url = f"https://api.openweathermap.org/data/3.0/onecall/timemachine?lat={lat}&lon={lon}&dt={time}&appid={api_key}&units={'imperial'}"
+    response = requests.get(base_url)
+    weather_data = response.json()
+    return weather_data
+
+
+
 
 if __name__ == "__main__":
-    # Inputs
-    file_input = input("What weather file would you like to open?\n")
+    liveData = input('Do you want to access live data?\n')
+    #API
+    if liveData.lower() == 'yes':
+        api_key = '5a614bbef05a248c1de15504b4d61ac1'
+        lat = input("Enter the city's latitude: ")
+        lon = input("Enter the city's longitude: ")
+        time = input('What time would you like to check: ')
+        weather_data = get_liveWeather_from_api(api_key, lat, lon, time)
+        print(weather_data)
+
+    #Non-API
+    else:
+        # Inputs
+        file_input = input("What weather file would you like to open?\n")
 
 
-    # Data and File Init
-    data = []
+        # Data and File Init
+        data = []
 
 
-    # Parsing data to display it
-    parse_data(file_input, data)
+        # Parsing data to display it
+        parse_data(file_input, data)
 
 
-    # Data Type Input
-    data_type = input("What data type would you like to get\n")
-    data_index = -1
+        # Data Type Input
+        data_type = input("What data type would you like to get\n")
+        data_index = -1
 
 
-    # Find Data Type
+        # Find Data Type
 
 
-    # Gets dates and their index
-    first_date = input("From what date (inclusive)\n")
-    end_date = input("To what date (inclusive)\n")
+        # Gets dates and their index
+        first_date = input("From what date (inclusive)\n")
+        end_date = input("To what date (inclusive)\n")
 
-    display_type = input("What data would you like (Max)(Min)(Average)(All)(Histogram)")
+        display_type = input("What data would you like (Max)(Min)(Average)(All)(Histogram)")
 
-    get_data(first_date, end_date, display_type, data_type, data)
+        get_data(first_date, end_date, display_type, data_type, data)
